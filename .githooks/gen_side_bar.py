56649a1f538ef6f6fd7ef27c54e8014d6272c0f8
returnwellbeing <returnwellbeing@gmail.com>
2018-08-22 23:16:07 +0900

    git wiki의 _Sidebar.md를 자동으로 생성한다. git commit 할때마다

---
+++ .githooks/gen_side_bar.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import urllib
+
+ROOT_DIR = "."
+EXCLUDE_LIST = ["Home.md", ".githooks", ".git", "_Sidebar.md"]
+WIKI_URL = "https://github.com/returnwellbeing/test_wiki/wiki"
+
+def traversal(root_dir, depth):
+    try:
+        unsorted_file_list = os.listdir(root_dir)
+        sub_dir_list = [] 
+        file_list = sorted(unsorted_file_list, key=lambda s: s.lower())
+
+        for file_name in file_list:
+            if file_name in EXCLUDE_LIST :
+                continue
+            
+            file_path = os.path.join(root_dir, file_name)
+            if os.path.isdir(file_path):
+                sub_dir_list.append(file_name)
+                continue
+            
+            name, ext = os.path.splitext(file_name)
+            if not (ext == ".md"):
+                continue
+
+            for i in range(0,depth): sys.stdout.write("\t")
+            abs_url = WIKI_URL + "/" + name
+            encoded_url = WIKI_URL + "/" + urllib.quote_plus(name) 
+            
+            sys.stdout.write("* [" + name + "](" + encoded_url + ")\n")
+
+        for sub_dir_name in sub_dir_list:
+            sub_dir_path = os.path.join(root_dir, sub_dir_name)
+            
+            for i in range(0,depth): sys.stdout.write("\t")
+            sys.stdout.write("* [" + sub_dir_name + "]\n")
+
+            traversal(sub_dir_path, depth+1)
+
+    except PermissionErr:
+        pass
+
+sys.stdout.write("[Home](" + WIKI_URL + "/Home)\n")
+
+traversal(ROOT_DIR, 0)
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-test
+# git wiki의 sidebar를 자동으로 생성해주는 스크립트
+
+* git clone https://github.com/returnwellbeing/test_wiki.git
+
+* cd test_wiki
+
+* cp -R .githooks {path/to/your_wiki.git}/.githooks
+
+* cd {path/to/your_wiki.git}
+
+* git config core.hooksPath .githooks
+
+* git commit 할때 마다 _Sidebar.md 내용을 업데이트 해준다
